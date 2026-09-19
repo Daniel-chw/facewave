78fbd0b88c1c44f9000fb2b50e94e7023d077034
Daniel-chw <danielcharlesworth26@gmail.com>
2026-09-19 14:43:58 +0100

    images can be saved and loaded

--- src/image.rs
+++ src/image.rs
@@ -1,13 +1,31 @@
 // handles loading and saving the image
 
 use crate::Image;
+use image::{DynamicImage, GrayImage, ImageReader};
 
-pub fn load_grayscale(path: &str) -> Image {
-    todo!()
+
+pub fn load_grayscale(path: &str) -> Result<Image, image::ImageError> {
+    let img = ImageReader::open(path)?.decode()?;
+    let gray = img.to_luma32f();
+
+    let w = gray.width() as usize;
+    let h = gray.height() as usize;
+    let data = gray.into_raw();
+
+    Ok(Image { w, h, data })
+
 }
 
-pub fn save(img: &Image, path: &str) -> std::io::Result<()> {
-    todo!()
+pub fn save(img: &Image, path: &str) -> image::ImageResult<()>  {
+    let w = img.w as u32;
+    let h = img.h as u32;
+    
+    let pixels: Vec<u8> = img.data.iter().map(|&v| (v*255.0) as u8).collect();
+
+    let buf = GrayImage::from_raw(w, h, pixels).unwrap();
+
+    buf.save(path)
+
 }
 
 pub fn psnr(a: &Image, b: &Image) -> f32 {
